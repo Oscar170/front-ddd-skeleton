9613cec7f523b6bcb18bf3121b0e7a335a19734e
Oscar Viciana <oscar@MBP-de-Oscar.technicolor.net>
2022-02-10 16:40:32 +0100

    Add weak password validation error

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "*.ts?(x)": () => "npm run typecheck",
+  "*.{ts,tsx,css,md}": (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(" --file ")}`,
+};
--- src/context/domain/auth/Password.ts
+++ src/context/domain/auth/Password.ts
@@ -1,7 +1,18 @@
+import { BaseError } from "@/utils/BaseError";
 import { Brand } from "@/utils/Brand";
 import { Either } from "@/utils/Either";
 
 export type PasswordValue = Brand<string, "password">;
 
-export const Password = (password: string): Either<Error, PasswordValue> =>
+export class WeakPasswordError extends BaseError<"WeakPasswordError"> {
+  constructor() {
+    super(
+      "Weak password is short, common, a system default, or something that could be rapidly guessed"
+    );
+  }
+}
+
+export const Password = (
+  password: string
+): Either<WeakPasswordError, PasswordValue> =>
   Either.of(password as PasswordValue);
